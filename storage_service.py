162efdeb93f66ba3a7a78fc06d16c4a0083b431d
AlexandraAndritoi <alexandraandritoi1@gmail.com>
2018-05-18 09:57:31 +0300

    Car moves backward after reaching the edge

--- storage_service.py
+++ storage_service.py
@@ -1,27 +1,45 @@
 from threading import Thread
 from functools import wraps
 import time
-import flask
 
 from car.car import Car
 from storage.storage_scan import StorageScan
 from storage.storage_object import StorageObject
 
 class StorageService:
-    def __init__(self, publish_distances_func):
+    def __init__(self, publish_distances_func, publish_location_func):
         self.publish_distances_func = publish_distances_func
+        self.publish_location_func = publish_location_func
         self.scan_interval = 0.25
         # self.car = Car(6, 13, 19, 26, 21, 20, 16, 12,
         #       14, 15, 18, 17, 2, 3)
-        #self.car = Car(13, 6, 26, 19, 20, 21, 12, 16,
-        #               14, 15, 18, 17, 2, 3)
         self.car = Car(17, 18, 22, 23, 5, 6, 12, 13,
-                  2, 3, 14, 15, 24, 25)
+                       2, 3, 14, 15, 24, 25)
 
         self.car_running = False
 
+    def _return_back_car(self, start_time, end_time):
+        print('Returning back the car ...')
+        duration = end_time - start_time
+        remaining_duration = end_time - start_time
+        step = 0.2
+
+        self.car.stop()
+        time.sleep(step)
+
+        self.car.move_backward()
+        while remaining_duration > 0:
+            remaining_duration = remaining_duration - step
+            location = remaining_duration / duration
+            print("Location: " + str(location))
+            self.publish_location_func(location)
+            time.sleep(step)
+
+        self.car.stop()
+
     def _scan_storage_worker(self):
         storage_scan = StorageScan(210, 50, self.scan_interval)
+        start_time = time.time()
 
         while self.car_running:
             dists = self.car.read_distances()
@@ -37,16 +55,21 @@ class StorageService:
 
             time.sleep(self.scan_interval)
 
+        end_time = time.time()
+
         objects = storage_scan.collect_objects()
         print('Number of scans: ' + str(storage_scan.no_scan_dists()) + '; number of objects: ' + str(len(objects)))
         for obj in objects:
             print('Start index: ' + str(obj.start_index) + '; end index: ' + str(obj.end_index))
 
+        self._return_back_car(start_time, end_time)
+
+
+
     def scan_storage(self):
         dists = self.car.read_distances()
         emergency_stop_front = dists[3]
-        emergency_stop_side = dists[4]
-        
+
         if emergency_stop_front:
             print('Too close to objects. Car not moving forward!')
             return
